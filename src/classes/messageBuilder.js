22cbeab534479796e25f49dcd25e9779fcc50e0e
Luiggi33 <github@luiggi33.de>
2024-09-11 22:36:09 +0200

    add warnings and truncation for too long values

--- src/classes/messageBuilder.js
+++ src/classes/messageBuilder.js
@@ -18,6 +18,11 @@ module.exports = class MessageBuilder {
     }
 
     setAuthor(author, authorImage, authorUrl){
+        const MAX_LENGTH = 256;
+        if (author.length > MAX_LENGTH) {
+            console.warn(`Author is too long. It has been truncated to ${MAX_LENGTH} characters.`);
+            author = author.substring(0, MAX_LENGTH);
+        }
         this.payload.embeds[0].author = {};
         this.payload.embeds[0].author.name = author;
         this.payload.embeds[0].author.url = authorUrl;   
@@ -27,6 +32,11 @@ module.exports = class MessageBuilder {
     };
 
     setTitle(title){
+        const MAX_LENGTH = 256;
+        if (title.length > MAX_LENGTH) {
+            console.warn(`Title is too long. It has been truncated to ${MAX_LENGTH} characters.`);
+            title = title.substring(0, MAX_LENGTH);
+        }
         this.payload.embeds[0].title = title;
 
         return this;
@@ -70,12 +80,31 @@ module.exports = class MessageBuilder {
     };
 
     setDescription(description){
+        const MAX_LENGTH = 4096;
+        if (description.length > MAX_LENGTH) {
+            console.warn(`Description is too long. It has been truncated to ${MAX_LENGTH} characters.`);
+            description = description.substring(0, MAX_LENGTH);
+        }
         this.payload.embeds[0].description = description;
 
         return this;
     };
 
     addField(fieldName, fieldValue, inline){
+        if (this.payload.embeds[0].fields.length >= 25){
+            console.warn('Fields limit reached. Ignoring field.');
+            return this;
+        }
+        const MAX_LENGTH_NAME = 256;
+        if (fieldName.length > MAX_LENGTH_NAME) {
+            console.warn(`Field name is too long. It has been truncated to ${MAX_LENGTH_NAME} characters.`);
+            fieldName = fieldName.substring(0, MAX_LENGTH_NAME);
+        }
+        const MAX_LENGTH_VALUE = 1024;
+        if (fieldValue.length > MAX_LENGTH_VALUE) {
+            console.warn(`Field value is too long. It has been truncated to ${MAX_LENGTH_VALUE} characters.`);
+            fieldValue = fieldValue.substring(0, MAX_LENGTH_VALUE);
+        }
         this.payload.embeds[0].fields.push({
             name: fieldName,
             value: fieldValue,
@@ -86,6 +115,11 @@ module.exports = class MessageBuilder {
     };
 
     setFooter(footer, footerImage){
+        const MAX_LENGTH = 2048;
+        if (footer.length > MAX_LENGTH) {
+            console.warn(`Footer is too long. It has been truncated to ${MAX_LENGTH} characters.`);
+            footer = footer.substring(0, MAX_LENGTH);
+        }
         this.payload.embeds[0].footer = {};
         this.payload.embeds[0].footer.icon_url = footerImage;
         this.payload.embeds[0].footer.text = footer;
